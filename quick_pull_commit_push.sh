be83721ee48eaf0b3746fce434cd021efd180876
Brian Miller <Brismiller@gmail.com>
2018-06-14 19:06:02 -0700

    test2

--- quick_pull_commit_push.sh
+++ quick_pull_commit_push.sh
@@ -2,11 +2,8 @@
 
 #  $1 = "message for commit"
 cwd=$(basename $PWD)  # gets name of project
-# pull_address=$(echo "https://github.com/BrianSMiller/$cwd.git")
 
-
-# echo $pull_address
 git pull https://github.com/BrianSMiller/$cwd.git master
 git add -A
 git commit --all -m $1
-# git push https://github.com/BrianSMiller/$cwd.git master
+git push https://github.com/BrianSMiller/$cwd.git master
