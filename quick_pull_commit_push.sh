899b704a11f8e3fd381d81a28cef118877080ec5
Brian Miller <Brismiller@gmail.com>
2018-06-14 18:42:02 -0700

    test2

--- quick_pull_commit_push.sh
+++ quick_pull_commit_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git pull https://github.com/BrianSMiller/coding_notes.git master
 git add -A
-git commit --all $1
+git commit --all -m $1
 git push https://github.com/BrianSMiller/coding_notes.git master
